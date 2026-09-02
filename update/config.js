5e9cf7c46e1d86c01b6944b7c299903a8f99c9ff
peterkvamme <peterkvamme@gmail.com>
2026-09-01 19:56:05 -0500

    Add files via upload

---
+++ update/config.js
@@ -0,0 +1,14 @@
+window.CAROUSEL_CONFIG = {
+  dayType: "A",
+  schedule: [
+    { name: "Period 1", time: "8:30-9:50", size: "main" },
+    { name: "Seminar", time: "9:55-10:30", size: "main" },
+    { name: "Period 2", time: "10:35-11:50", size: "main" },
+    { name: "Period 3", time: "11:55-1:40", size: "main" },
+    { name: "Lunch A", time: "11:55-12:20", size: "lunch" },
+    { name: "Lunch B", time: "12:35-1:00", size: "lunch" },
+    { name: "Lunch C", time: "1:15-1:40", size: "lunch" },
+    { name: "Period 4", time: "1:45-3:00", size: "main" }
+  ],
+  notifications: [{ date: "2026-09-08", text: "Welcome back!" }]
+};
