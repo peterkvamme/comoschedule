63d6cea621f2437980018d3738fd0445b6d44568
peterkvamme <peterkvamme@gmail.com>
2026-09-01 20:15:17 -0500

    Add files via upload

--- update/config.js
+++ update/config.js
@@ -1,15 +1,1 @@
-window.CAROUSEL_CONFIG = {
-  dayType: "A",
-  heading: "A DAY",
-  schedule: [
-    { name: "Period 1", time: "8:30-9:50", size: "main" },
-    { name: "Seminar", time: "9:55-10:30", size: "main" },
-    { name: "Period 2", time: "10:35-11:50", size: "main" },
-    { name: "Period 3", time: "11:55-1:40", size: "main" },
-    { name: "Lunch A", time: "11:55-12:20", size: "lunch" },
-    { name: "Lunch B", time: "12:35-1:00", size: "lunch" },
-    { name: "Lunch C", time: "1:15-1:40", size: "lunch" },
-    { name: "Period 4", time: "1:45-3:00", size: "main" }
-  ],
-  notifications: [{ date: "2026-09-08", text: "Welcome back!" }]
-};
+window.CAROUSEL_CONFIG={notifications:[{date:"2026-09-09",dayType:"A",heading:"Wednesday 9/9",schedule:[{name:"EAs/TAs Room 1312",time:"11:00 - 3:00",size:"main"},{name:"Nuts and Bolts (Library) All Staff",time:"1:00 - 3:00",size:"main"},{name:"Open House",time:"5:00 - 7:00",size:"main"}]}]};
